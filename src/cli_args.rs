cefcae027d52b458c5a13cf960d1b51d7156fbfa
Michael Freimüller <michael@mfreimueller.com>
2025-10-17 12:20:21 +0200

    add help message to existing cli args

--- src/cli_args.rs
+++ src/cli_args.rs
@@ -3,8 +3,8 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Args {
-    #[arg(short, long, default_value = "")]
+    #[arg(short, long, default_value = "", help = "The start date used to calculate differences from. The default value is yesterdays' date, therefor the change set since yesterday is used.")]
     pub(crate) date: String,
-    #[arg(short, long, default_value = "repograde.toml")]
+    #[arg(short, long, default_value = "repograde.toml", help = "The location of the config file.")]
     pub(crate) config_file: String,
 }
